98310464951752b37359557bf657b913f20d9e09
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-03 00:52:55 +0530

    refactor Ascci Art File

---
+++ credentials/arts.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void Credentials() {
+    int number;
+
+    printf("> 1. Login \n");
+    printf("> 2. Register \n\n");
+    printf("> Enter the number & hit ENTER: ");
+    scanf("%d", &number);
+
+    switch (number) {
+        case (1):
+            printf("Login");
+            break;
+        case (2):
+            printf("Register as,\n");
+            printf("> 1. Teacher \n");
+            printf("> 2. Student \n");
+            printf("> 3. Staff Member \n");
+            printf("> Enter the number & hit ENTER: ");
+            scanf("%d", &number);
+            switch (number) {
+                case 1:
+                    printf("Enter Username:")
+                case 2:
+                case 3:
+
+
+            }
+
+
+
+
+            break;
+
+    }
+
+}

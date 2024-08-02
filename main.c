8187308ca3533bc7cb2ae41bf3294b7286300a41
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-03 00:53:04 +0530

    refactor Ascci Art File

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include "ascciarts/arts.c"
+#include "credentials/arts.c"
+
 /*
 int main() {
 //    FILE *file;
@@ -18,7 +21,6 @@ int main() {
 }
 */
 
-#include <stdio.h>
 //
 //int main() {
 //    char strArray[256]; // Declare a strArray with a size of 256 bytes
@@ -34,19 +36,8 @@ int main() {
 //}
 
 
-#include <stdio.h>
-
 int main() {
-    printf("╔───────────────────────────────────────────────────────────────────────────╗\n");
-    printf("│      .o.            88 ooooo             ooo        ooooo       .oooooo..o│\n");
-    printf("│     .888.          .8' `888'             `88.       .888'      d8P'    `Y8│\n");
-    printf("│    .8\"888.        .8'   888               888b     d'888       Y88bo.     │\n");
-    printf("│   .8' `888.      .8'    888               8 Y88. .P  888        `\"Y8888o. │\n");
-    printf("│  .88ooo8888.    .8'     888               8  `888'   888            `\"Y88b│\n");
-    printf("│ .8'     `888.  .8'      888       o       8    Y     888       oo     .d8P│\n");
-    printf("│o88o     o8888o 88      o888ooooood8      o8o        o888o      8""88888P'   │\n");
-    printf("╚───────────────────────────────────────────────────────────────────────────╝\n");
-
-
+    IntroAscciArt();
+    Credentials();
     return 0;
 }

ce8208387f4cc9adb8f87cdf43f2dcd2147d863b
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-03 02:58:06 +0530

    Update main.c
    Remove unnecessary code
    
    Co-Authored-By: Aruna Kaushalya <arunakaushalya62@gmail.com>

--- main.c
+++ main.c
@@ -2,40 +2,6 @@
 #include "ascciarts/arts.c"
 #include "credentials/credentials.c"
 
-/*
-int main() {
-//    FILE *file;
-//    file = fopen("nonexistentfile.txt", "r");
-//
-//    if (file == NULL) {
-//        printf(stderr, "Error: Could not open file\n");
-//        return 1;
-//    }
-//
-//    // Further file operations
-//    fclose(file);
-
-
-
-    return 0;
-}
-*/
-
-//
-//int main() {
-//    char strArray[256]; // Declare a strArray with a size of 256 bytes
-//
-//    // Read data into the strArray from stdin
-//    printf("Enter some text: ");
-//    fgets(strArray, sizeof(strArray), stdin); // Reading from stdin to strArray
-//
-//    // Output the data stored in the strArray
-//    printf("You entered: %s", strArray); // Writing from strArray to stdout
-//
-//    return 0;
-//}
-
-
 int main() {
     IntroAscciArt();
     Credentials();

9fc3e81bbdd99870a4f4b42c4cbf4f3d3c3d81f5
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-02 23:06:41 +0530

    final commit

--- main.c
+++ main.c
@@ -19,16 +19,40 @@ int main() {
 */
 
 #include <stdio.h>
+//
+//int main() {
+//    char strArray[256]; // Declare a strArray with a size of 256 bytes
+//
+//    // Read data into the strArray from stdin
+//    printf("Enter some text: ");
+//    fgets(strArray, sizeof(strArray), stdin); // Reading from stdin to strArray
+//
+//    // Output the data stored in the strArray
+//    printf("You entered: %s", strArray); // Writing from strArray to stdout
+//
+//    return 0;
+//}
+
+
+#include <stdio.h>
 
 int main() {
-    char strArray[256]; // Declare a strArray with a size of 256 bytes
+    int array[] = {1, 1, 1, 1, 1};
+    int length = sizeof(array) / sizeof(array[0]);
+    int sum = 0;
 
-    // Read data into the strArray from stdin
-    printf("Enter some text: ");
-    fgets(strArray, sizeof(strArray), stdin); // Reading from stdin to strArray
+    // Calculate the sum of the array elements
+    for (int i = 0; i < length; i++) {
+        sum += array[i];
+    }
 
-    // Output the data stored in the strArray
-    printf("You entered: %s", strArray); // Writing from strArray to stdout
+    printf("Sum of array elements: %d\n", sum);
+
+    // Calculate and print the percentage of each element
+    for (int i = 0; i < length; i++) {
+        float percentage = ((float)array[i] / sum) * 100;
+        printf("Element %d: %.2f%%\n", array[i], percentage);
+    }
 
     return 0;
 }

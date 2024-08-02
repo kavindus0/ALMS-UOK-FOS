6ca01a49a8d49b7250df49d178ee6d14d66c3338
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-02 23:57:29 +0530

    basic interface making

--- main.c
+++ main.c
@@ -37,22 +37,16 @@ int main() {
 #include <stdio.h>
 
 int main() {
-    int array[] = {1, 1, 1, 1, 1};
-    int length = sizeof(array) / sizeof(array[0]);
-    int sum = 0;
+    printf("╔───────────────────────────────────────────────────────────────────────────╗\n");
+    printf("│      .o.            88 ooooo             ooo        ooooo       .oooooo..o│\n");
+    printf("│     .888.          .8' `888'             `88.       .888'      d8P'    `Y8│\n");
+    printf("│    .8\"888.        .8'   888               888b     d'888       Y88bo.     │\n");
+    printf("│   .8' `888.      .8'    888               8 Y88. .P  888        `\"Y8888o. │\n");
+    printf("│  .88ooo8888.    .8'     888               8  `888'   888            `\"Y88b│\n");
+    printf("│ .8'     `888.  .8'      888       o       8    Y     888       oo     .d8P│\n");
+    printf("│o88o     o8888o 88      o888ooooood8      o8o        o888o      8""88888P'   │\n");
+    printf("╚───────────────────────────────────────────────────────────────────────────╝\n");
 
-    // Calculate the sum of the array elements
-    for (int i = 0; i < length; i++) {
-        sum += array[i];
-    }
-
-    printf("Sum of array elements: %d\n", sum);
-
-    // Calculate and print the percentage of each element
-    for (int i = 0; i < length; i++) {
-        float percentage = ((float)array[i] / sum) * 100;
-        printf("Element %d: %.2f%%\n", array[i], percentage);
-    }
 
     return 0;
 }

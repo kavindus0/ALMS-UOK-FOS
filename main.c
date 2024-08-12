cb4b770d6a27257d8ab09c7969c9c9d7d6e5665b
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-12 19:48:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <json-c/json.h>
+// #include <json-c/json.h>
 #include "ascciarts/arts.c"
 #include "credentials/credentials.c"
 #include "credentials/credentials.c"
 
-int main() {
+int main()
+{
     IntroAscciArt();
     Credentials();
     return 0;

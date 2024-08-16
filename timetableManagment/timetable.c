7ddf204d704d98f9c46eaead689e9b6382dff543
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-17 01:02:46 +0530

    Update timetable.c

--- timetableManagment/timetable.c
+++
@@ -1,7 +0,0 @@
-void editSubjectDetails(Subject *subject)
-{
-    printf("Enter new subject name: ");
-    scanf("%s", subject->name);
-    printf("Enter new teacher name: ");
-    scanf("%s", subject->teacher);
-}

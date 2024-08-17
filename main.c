6d3ee5fbc15db3add524aa3f5705af9108acaf95
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-17 21:19:08 +0530

    remove unnesessary struct

--- main.c
+++ main.c
@@ -1,15 +1,10 @@
-#include <stdio.h>
-// #include <json-c/json.h>
+
 #include "ascciarts/arts.c"
 #include "credentials/credentials.c"
 
 
 int main()
 {
-    struct liveuser{
-        char name[50];
-        char role[50];
-    };
    // while (1==1) {
         IntroAscciArt();
         Credentials();

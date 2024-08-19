ab9b5f0f2ad22aa640a16349dbe334f60e394ec4
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-19 23:38:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -8,16 +8,7 @@ int main()
     struct User temp;
    // while (1==1) {
         IntroAscciArt();
-        temp = Credentials();
-        if (temp.role == "Teacher") {
-            printf("Welcome Teacher %s\n", temp.username);
-        } else if (temp.role == "Student") {
-            printf("Welcome Student %s\n", temp.username);
-        } else if (temp.role == "Staff Member") {
-            printf("Welcome Staff Member %s\n", temp.username);
-        } else if(temp.role == NULL) {
-            printf("Please Retry %s\n", temp.username);
-        }
+        Credentials();
 
     return 0;
 }

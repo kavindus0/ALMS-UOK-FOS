796b62792d0d4875758c3727a5feedd53c918681
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-20 00:17:11 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,7 +1,8 @@
-//
-// Created by kavindu on 2024-07-29.
-//
-
 #ifndef SEM_01_C_MAIN_H
 #define SEM_01_C_MAIN_H
-#endif //SEM_01_C_MAIN_H
+
+void TeachersThing();
+void StudentsThing();
+void StaffThing();
+
+#endif // SEM_01_C_MAIN_H

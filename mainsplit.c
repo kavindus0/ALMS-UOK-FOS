2adc02c69a5e615b134f994ed82e07437bfed732
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-20 00:39:13 +0530

    Update mainsplit.c

--- mainsplit.c
+++ mainsplit.c
@@ -7,7 +7,34 @@
 
 
 void TeachersThing(){
-    printf("Teachers Thing");
+
+void EDUconnectCommunicationPortal();
+void HR();
+void ExtraActivities();
+void TimeTable();
+
+     int input;
+    printf("\nEnter a number:\n1. EDUconnect Communication Portal\n2. HR\n3. Extra Activities\n4. Time Table\n");
+    scanf("%d", &input);
+        switch (input) {
+        case 1:
+            EDUconnectCommunicationPortal();
+            break;
+        case 2:
+            HR();
+            break;
+        case 3:
+            ExtraActivities();
+            break;
+        case 4:
+            TimeTable();
+            break;
+        default:
+            printf("Invalid input\n");
+            break;
+    }
+
+
 }
 
 
@@ -19,4 +46,22 @@ void StudentsThing(){
 void StaffThing(){
     printf("Teachers Thing");
 
+}
+
+
+
+void EDUconnectCommunicationPortal() {
+    printf("EDUconnect Communication Portal\n");
+}
+
+void HR() {
+    printf("HR\n");
+}
+
+void ExtraActivities() {
+    printf("Extra Activities\n");
+}
+
+void TimeTable() {
+    printf("Time Table\n");
 }

4b8cd498d294404eeb60d88f8a3e30d21404e30d
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-20 00:17:17 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,15 @@
 
 #include "ascciarts/arts.c"
 #include "credentials/credentials.c"
+#include <stdio.h>
+
 
 
 int main()
 {
     struct User temp;
-   // while (1==1) {
         IntroAscciArt();
         Credentials();
-
     return 0;
 }
 
-void TeachersThing(){
-    printf("Teachers Thing");
-}
-
-
-void StudentsThing(){
-    
-}
-
-void StaffThing(){
-    
-}

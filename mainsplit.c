825724f5a3b5165c944a88e61c2dd8e965dce367
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-20 00:17:08 +0530

    Create mainsplit.c

---
+++ mainsplit.c
@@ -0,0 +1,21 @@
+//
+// Created by kavindu on 2024-08-20.
+//
+#import <stdio.h>
+#import <string.h>
+
+
+void TeachersThing(){
+    printf("Teachers Thing");
+}
+
+
+void StudentsThing(){
+    printf("Teachers Thing");
+
+}
+
+void StaffThing(){
+    printf("Teachers Thing");
+
+}

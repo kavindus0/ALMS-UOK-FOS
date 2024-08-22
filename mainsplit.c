42ccebe30234c39e67c975b77241080e88599a1e
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-22 13:52:31 +0530

    Update mainsplit.c

--- mainsplit.c
+++ mainsplit.c
@@ -7,30 +7,25 @@ struct User {
     char role[20];
 };
 
-void TeachersThing(struct User *user);
-
+void TeachersThing(char *user);
 void StudentsThing();
 void StaffThing();
-
 void HR();
 void ExtraActivities();
 void TimeTable();
-void EDUcom(struct User *user);
+void EDUcom(char *user);
 
-//int main() {
-//    struct User user = {"kavindu", "password123", "teacher"};
-//    TeachersThing(&user);
-//    return 0;
-//}
 
-void TeachersThing(struct User *user) {
-    printf("%s", *user->username);
+void TeachersThing(char *user) {
+
+
     int input;
     printf("\n1. EDUconnect Communication Portal\n2. HR\n3. Extra Activities\n4. Time Table\n\nEnter a number: ");
     scanf("%d", &input);
     switch (input) {
         case 1:
             EDUcom(user);
+            printf("%s\n", user);
             break;
         case 2:
             HR();
@@ -67,8 +62,9 @@ void TimeTable() {
     printf("Time Table\n");
 }
 
-void EDUcom(struct User *user) {
+void EDUcom(char *user) {
     printf("\n---- EDUconnect Communication Portal -----\n");
+    printf("\n---- Logged As %s -----\n",user);
     printf("1. School Announcement\n");
     printf("2. Open Portal\n");
     printf("Enter a number and Hit ENTER: ");
@@ -77,8 +73,18 @@ void EDUcom(struct User *user) {
     switch (input) {
         case 1:
             printf("\nSchool Announcement Logged\n");
-            printf("\nSend a Notification to Announcement\n");
-            printf("%s hiii\n", user->username);
+            printf("\nWrite And HIT ENTRE to Send a Notification to Announcement\n");
+            char announcement[100];
+            scanf("%s", announcement);
+            FILE *fptrForAnnounce = fopen("./communication/SchoolAnnouncement/index.html","a");
+    if (fptrForAnnounce == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+    fprintf(fptrForAnnounce, "%s", announcement);
+
+            printf("%s Successfully Printed\n", user);
+            fclose(fptrForAnnounce);
             break;
         case 2:
             printf("\nOpen Portal Logged\n");

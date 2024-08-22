b31cd59233cfdf1d3ac3eb53d36fb43148952e1f
kavindus0 <126804361+kavindus0@users.noreply.github.com>
2024-08-22 16:09:51 +0530

    code formatted

--- mainsplit.c
+++ mainsplit.c
@@ -185,3 +185,45 @@ void EDUcomStaff(char *user) {
             break;
     }
 }
+void EDUcomStud(char *user) {
+    printf("\n---- EDUconnect Communication Portal -----\n");
+    printf("\n---- Logged As %s -----\n", user);
+    printf("1. Open Portal\n");
+    printf("Enter a number and Hit ENTER: ");
+    int input;
+    scanf("%d", &input);
+    switch (input) {
+        case 1:
+            printf("\nSchool Open Portal Logged\n");
+            printf("\nWrite And HIT ENTRE to Send a Notification to Announcement\n");
+            char announcement[10000];
+            getchar(); // To consume the newline character left by previous scanf
+            fgets(announcement, sizeof(announcement), stdin);
+            FILE *fptrForAnnounce = fopen("./communication/SchoolAnnouncement/index.html", "a");
+            if (fptrForAnnounce == NULL) {
+                printf("Error opening file!\n");
+                return;
+            }
+            time_t t;
+            struct tm *tm_info;
+            time(&t);
+            tm_info = localtime(&t);
+            char buffer[26];
+            strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+            // printf("Current Date and Time: %s\n", buffer);
+            fprintf(fptrForAnnounce, "<div class=\"message-bubble-stu\">\n");
+            fprintf(fptrForAnnounce, "    <div class=\"sender-name\">%s</div>\n", user);
+            fprintf(fptrForAnnounce, "    <div class=\"message-body\">%s</div>\n", announcement);
+            fprintf(fptrForAnnounce, "    <div class=\"message-time\">%s</div>\n", buffer);
+            fprintf(fptrForAnnounce, "</div>\n");
+            printf("Message Successfully Sent at : %s\n", buffer);
+            fclose(fptrForAnnounce);
+            break;
+        case 2:
+            printf("\nOpen Portal Logged\n");
+            break;
+        default:
+            printf("\nInvalid input\n");
+            break;
+    }
+}

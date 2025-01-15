2cded39b54ca5ab4fed108a100dd33d6eb1691f7
Kavindu Sachinthe <126804361+kavindus0@users.noreply.github.com>
2025-01-15 18:00:35 +0530

    herader comment added

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+/*
+ * ADVANCED LEVEL SECTION
+MANAGEMENT SYSTEM
+
+ Group 06
+
+ K.S.B. Galkotuwa
+ EC/2022/053
+ galkotu-ec22053@stu.kln.ac.lk
+
+
+ */
+
+
+
 #include "ascciarts/arts.c"
 #include "credentials/credentials.c"
 #include "mainsplit.c"
